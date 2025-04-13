873ca06d433560718ecbb03aaa6d8e960536e992
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-13 21:58:29 +0500

    Task7

---
+++ Tyuiu.KardonKD.Sprint3.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KardonKD.Sprint3.Task7.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Cos(x) + (4 * x) / 2 - Math.Sin(x) * 3 * x), 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.KardonKD.Sprint3.Task7.V25/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.KardonKD.Sprint3.Task7.V25.Lib;
+namespace Tyuiu.KardonKD.Sprint3.Task7.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Кордон К.Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Кордон Ксения Дмитриевна | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу знаяений функции:         *");
+            Console.WriteLine("* F(x) = cos(x) + 4*x/2 - sin(x) * 3 * x (произвести табулирование)     *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага =  " + stopValue);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("+-------------+-------------+");
+            Console.WriteLine("|      X      |     f(x)    |");
+            Console.WriteLine("+-------------+-------------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("| {0,6:d}      |   {1,6:f2}    |", startValue, valueArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+-------------+-------------+");
+            Console.ReadKey();
+        }
+    }
+}
