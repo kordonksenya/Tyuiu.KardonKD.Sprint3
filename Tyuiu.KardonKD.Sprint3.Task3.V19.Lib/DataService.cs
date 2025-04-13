801149fc6b4d4cbc0f584a2392c364b4f687fdc5
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-13 20:09:19 +0500

    Task3

--- Tyuiu.KardonKD.Sprint3.Task3.V19.Lib/DataService.cs
+++ Tyuiu.KardonKD.Sprint3.Task3.V19.Lib/DataService.cs
@@ -1,6 +1,7 @@
-﻿namespace Tyuiu.KardonKD.Sprint3.Task3.V19.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KardonKD.Sprint3.Task3.V19.Lib
 {
-    public class DataService
+    public class DataService : ISprint3Task3V19
     {
         public string ReplaceCharOnNum(string value, char replaceable, char replacement)
         {
